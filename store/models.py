1cb544d2af9fa210a27b0fb72d9840acc2580d5b
Franky170504 <laukiksuryavanshi17@gmail.com>
2025-04-23 20:09:52 +0530

    models

--- store/models.py
+++ store/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+class Customers(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    mobile_no = models.IntegerField(max_length=10)
+    email = models.EmailField(max_length=100)
+    password = models.CharField
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
+class Products(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default= 1)
+    description = models.CharField(max_length=500)
+    image = models.ImageField(upload_to='uploads/products/')
+
+    def __str__(self):
+        return self.name
+    
+
