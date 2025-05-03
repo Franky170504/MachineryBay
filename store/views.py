b8583bfa5e3a9cf80fb0e095f702a2db8e7521ca
Laukik Suryavanshi <164608237+Franky170504@users.noreply.github.com>
2025-05-03 19:02:29 +0530

    store restored and filters added and landing page added

--- store/views.py
+++ store/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Products
 
 def home(request):
-    # products = Product.products.all()
-    return render(request, 'store\home.html', {})
+    # products = Products.products.all()
+    return render(request, 'store/home.html', {})#'products': products in curl brackets
+
+def landing(request):
+    return render(request, 'store/landing.html', {})
