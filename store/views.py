b4c82ad6ef1e22d4932c883adb811bcaa247df91
Laukik Suryavanshi <164608237+Franky170504@users.noreply.github.com>
2025-06-12 13:48:24 +0530

    new

--- store/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Products
-
-def home(request):
-    # products = Products.products.all()
-    return render(request, 'store/home.html', {})#'products': products in curl brackets
-
-def landing(request):
-    return render(request, 'store/landing.html', {})
